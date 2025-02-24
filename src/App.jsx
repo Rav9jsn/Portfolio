048c8b4923d2123b3808b6e0c08e940701ad2004
Rav9jsn <kravi402253@gmail.com>
2025-02-24 14:08:59 +0530

    upward button added

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ import Projects from "./Components/Projects";
 import Experience from "./Components/Experience";
 import Contact from "./Components/Contact";
 import { useState } from "react";
+import { IoMdNavigate } from "react-icons/io";
 
 const App = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
   const [bgStyle, setBgStyle] = useState(
     <div class="relative h-full w-full bg-black">
       <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
@@ -20,6 +24,13 @@ const App = () => {
 
       <div className=" container mx-auto px-8">
         <Navbar setBgStyle={setBgStyle} />
+        <div
+          onClick={scrollToTop}
+          className="py-1 fixed  bg-gradient-to-r from-blue-100 to-transparent top-[80vh] left-[80vw] lg:top-[90vh] lg:left-[90vw] text-black text-5xl rounded-full flex justify-center w-14  "
+        >
+          {" "}
+          <IoMdNavigate />{" "}
+        </div>
         <Hero />
         <Technologies />
         <Projects />
